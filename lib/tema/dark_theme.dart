2715fec1342ac2a858abe3d2b450b246308725b4
Christy <118579935+chrstyy@users.noreply.github.com>
2024-06-22 12:25:39 +0700

    k

--- lib/tema/dark_theme.dart
+++ lib/tema/dark_theme.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 final ThemeData darkTheme = ThemeData(
   primaryColor: const Color(0xFF93A1B1), //judul
   brightness: Brightness.dark,
-  scaffoldBackgroundColor: Color.fromARGB(133, 6, 6, 6),//container awl msuk/dftr, dn background pages lain
-  appBarTheme: AppBarTheme(
+  scaffoldBackgroundColor: const Color.fromARGB(133, 6, 6, 6),//container awl msuk/dftr, dn background pages lain
+  appBarTheme: const AppBarTheme(
     backgroundColor: Color(0xFF21262B),
     // backgroundColor:  Color.fromARGB(136, 0, 0, 0), //background awl msuk/dftr
     iconTheme: IconThemeData(color: Color(0xFF93A1B1)), //icon
   ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
     backgroundColor: Color(0xFF303239), //button dpn masuk/daftar &label atas
   ),
-  textTheme: TextTheme(
+  textTheme: const TextTheme(
     bodyText1: TextStyle(color: Color(0xFF93A1B1)), //text deskripsi
   ),
-  backgroundColor: Color.fromARGB(255, 37, 42, 51), //navigasi bwh
+  backgroundColor: const Color.fromARGB(255, 37, 42, 51), //navigasi bwh
 );
